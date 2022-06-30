8ed947b24fab0dffc5901609cbbf656e1f377ed1
lzc-cpu <648068096@qq.com>
2022-06-30 16:18:49 +0800

    commit

--- tileGrid.js
+++
@@ -1,7 +0,0 @@
-var osmSource = new ol.source.OSM();
-var GridTile = new ol.layer.Tile({
-    source: new ol.source.TileDebug({
-        projection: 'EPSG:3857', //地图投影坐标系
-        tileGrid: osmSource.getTileGrid()  //获取瓦片图层数据对象（osmSource）的网格信息
-    })
-})
